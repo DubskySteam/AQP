e5148cc80d16e5eabebc719c2955410da91e0c4a
Eric <soprah@gmx.de>
2023-11-23 19:46:32 +0100

    Added temporary solution with no swac API

--- frontend/SWAC/sites/register/register.js
+++ frontend/SWAC/sites/register/register.js
@@ -1,3 +1,4 @@
+/*
 var create_user_options = {
     showWhenNoData: true,
     allowAdd: true,
@@ -8,3 +9,37 @@ var create_user_options = {
         window.location = 'regconfirm.html';
     }
 }; 
+*/
+
+const fluppy = () => {
+    const url = 'http://localhost:8080/SmartUser/smartuser/user';
+
+    let username = document.getElementById('username').value;
+    let password = document.getElementById('password').value;
+    let privacy_accepted = document.getElementById('privacy_accepted').checked;
+    let terms_accepted = document.getElementById('terms_accepted').checked;
+
+    const data = {
+    username: username,
+    password: password,
+    privacy_accepted: privacy_accepted,
+    terms_accepted: terms_accepted
+    };
+
+
+    fetch(url, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(data)
+    })
+    .then(response => response.json())
+    .then(data => {
+        console.log('Success:', data);
+    })
+    .catch((error) => {
+        console.error('Error:', error);
+    });
+}
+
