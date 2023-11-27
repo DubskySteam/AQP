6d8b24c2c85a72e448fbd3108d0fc39688ade704
Dubsky <23001099+DubskySteam@users.noreply.github.com>
2023-11-27 23:33:52 +0100

    UserDTO now includes display_name

--- backend/projects/SmartSocial/src/main/java/de/hsbi/smartsocial/Model/User.java
+++ backend/projects/SmartSocial/src/main/java/de/hsbi/smartsocial/Model/User.java
@@ -14,12 +14,23 @@ public class User {
     @Column(name = "id", nullable = false)
     private Long id;
 
+    @Column(name = "display_name", nullable = false)
+    private String displayName;
+
     public Long getId() {
         return id;
     }
 
     public void setId(Long id) {
         this.id = id;
+    }
+
+    public String getDisplayName() {
+        return displayName;
+    }
+
+    public void setDisplayName(String displayName) {
+        this.displayName = displayName;
     }
 
 }
