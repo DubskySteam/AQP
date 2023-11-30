13d4a6e20afa14f047ed7176a97f71e69bf11abf
Dubsky <23001099+DubskySteam@users.noreply.github.com>
2023-11-30 14:37:52 +0100

    added endpoints to new ressource handler

--- backend/projects/SmartSocial/src/main/java/de/hsbi/smartsocial/RestEntry.java
+++ backend/projects/SmartSocial/src/main/java/de/hsbi/smartsocial/RestEntry.java
@@ -1,6 +1,6 @@
 package de.hsbi.smartsocial;
 
-import de.hsbi.smartsocial.Controller.GroupController;
+import de.hsbi.smartsocial.Controller.*;
 import io.swagger.v3.jaxrs2.integration.resources.OpenApiResource;
 import jakarta.ws.rs.ApplicationPath;
 import jakarta.ws.rs.core.Application;
@@ -16,6 +16,11 @@ public class RestEntry extends Application {
         Set<Class<?>> resources = new HashSet<>();
 
         resources.add(GroupController.class);
+        resources.add(LeaderboardController.class);
+        resources.add(AchievementController.class);
+        resources.add(QuestController.class);
+        resources.add(TestController.class);
+        resources.add(ProfileSettingsController.class);
         //resources.add(OpenApiResource.class);
 
         return resources;
