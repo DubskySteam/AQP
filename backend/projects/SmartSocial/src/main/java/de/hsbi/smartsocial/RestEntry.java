7bf709bbd8b324eda8a1cf638f03eb14967461d1
Dubsky <23001099+DubskySteam@users.noreply.github.com>
2023-11-30 14:24:57 +0100

    analysed bug to be openapi ressource

--- backend/projects/SmartSocial/src/main/java/de/hsbi/smartsocial/RestEntry.java
+++ backend/projects/SmartSocial/src/main/java/de/hsbi/smartsocial/RestEntry.java
@@ -1,9 +1,24 @@
 package de.hsbi.smartsocial;
 
+import de.hsbi.smartsocial.Controller.GroupController;
+import io.swagger.v3.jaxrs2.integration.resources.OpenApiResource;
 import jakarta.ws.rs.ApplicationPath;
 import jakarta.ws.rs.core.Application;
 
+import java.util.HashSet;
+import java.util.Set;
+
 @ApplicationPath("/api")
 public class RestEntry extends Application {
+
+    @Override
+    public Set<Class<?>> getClasses() {
+        Set<Class<?>> resources = new HashSet<>();
+
+        resources.add(GroupController.class);
+        //resources.add(OpenApiResource.class);
+
+        return resources;
+    }
 
 }
