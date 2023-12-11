7340cad7c8d3a6b69f390f0f84aae1c8414ca5d9
Eric <soprah@gmx.de>
2023-12-11 22:49:11 +0100

    Displaying data from fictional db table. Styles are done. Missing sort functionality.

--- frontend/SWAC/sites/rewards/rewards.js
+++ frontend/SWAC/sites/rewards/rewards.js
@@ -1,112 +1,86 @@
-/*
-document.addEventListener('DOMContentLoaded', async (event) => {
-    // Annahme: Ihre JSON-Daten sind in rewards_data.json vorhanden
-    const response = await fetch('/frontend/SWAC/sites/rewards/rewards_data.json');
-    const jsonData = await response.json();
+data = [];
 
-    jsonData.forEach((data, index) => {
-        var reward = document.getElementsByClassName('reward')[index];
-        var rewardTitleElement = reward.querySelector('.title');
-        var rewardMedalElement = reward.querySelector('.medal');
-        var rewardBesitzElement = reward.querySelector('.besitz');
-
-        // Überprüfen, ob die Elemente gefunden wurden
-        if (rewardTitleElement && rewardMedalElement && rewardBesitzElement) {
-            var rewardTitle = data.title;
-            var rewardMedal = data.medal;
-            var rewardBesitz = data.besitz;
-
-            var updatedContent = `
-                <h2>${rewardTitle}</h2>
-                <p><strong>Medaille:</strong> ${rewardMedal}</p>
-                <p><strong>Besitz:</strong> ${rewardBesitz === true ? 'Ja' : 'Nein'}</p>
-            `;
-
-            if (rewardBesitz === true) {
-                reward.style.backgroundColor = '#398EDE';
-                reward.style.color = 'white';
-            }
-
-            reward.innerHTML = updatedContent;
-        } else {
-            console.log('Fehler: Elemente nicht gefunden.');
-        }
-    });
-});
-*/
-
-/*
-document.addEventListener('DOMContentLoaded', () => {
-    var rewardsData = [];
-
-    var rewardElements = document.getElementsByClassName('reward');
-
-    for (var i = 0; i < rewardElements.length; i++) {
-        var reward = rewardElements[i];
-
-        // Direkt auf die Kinder zugreifen
-        var rewardTitleElement = reward.querySelector('.title').innerText;
-        var rewardMedalElement = reward.querySelector('.medal').innerText;
-        var rewardBesitzElement = reward.querySelector('.besitz').innerText.toLowerCase() === 'true';
-
-        var rewardData = {
-            title: rewardTitleElement,
-            medal: rewardMedalElement,
-            besitz: rewardBesitzElement
-        };
-
-        rewardsData.push(rewardData);
-    }
-
-    // Jetzt haben Sie die Daten in rewardsData und können darauf zugreifen.
-    console.log('Rewards Daten:', rewardsData);
-
-    // Hier können Sie weitere Aktionen mit den Daten durchführen, z.B. Anzeige, Filterung usw.
-    rewardsData.forEach(reward => {
-        console.log('Title:', reward.title);
-        console.log('Medal:', reward.medal);
-        console.log('Besitz:', reward.besitz);
-        console.log('-----------------');
-    });
-
-    // Hier können Sie auch Ihre Funktion changeInnerHTML anrufen und das rewardsData-Array übergeben.
-    // changeInnerHTML(rewardsData);
-});
-*/
-
-/*
-document.addEventListener('DOMContentLoaded', async function(){
-    setTimeout(async function(){
-        var rewardElements = document.getElementsByClassName('reward');
-        var firstElement = rewardElements[0]
-        var medalValue = firstElement.querySelector('.medal').innerHTML;
-        console.log(medalValue);
-
-
-    }, 1000);
-});
-*/
-
-        /*
-        var s = document.getElementById('present_example6');
-        var a = s.swac_comp.getChilds("swac_repeatForSet");
-        console.log(a[0]);
-        */
 document.addEventListener('swac_ready', function () {
     window.swac.reactions.addReaction(function (requestors) {
-        // Get the component element
-        mycompelem = requestors["present_example6"];
-        // Get the component data array
-        let datasources = mycompelem.swac_comp.data;
-        // Get a datasource that name you know (else iterate through the array)
-        let mydatasource = datasources['/frontend/SWAC/sites/rewards/rewards_data.json'];
-        // Get sets
-        let mydatasets = mydatasource.getSets();
-        console.log("Proxy Test", mydatasets);
-        // Access set with it 2
-        let set2 = mydatasets[2];
-        console.log("Datasources:" + datasources);
-        console.log(datasources);
-        console.log("test3", set2.besitz);
+        comp = requestors["present_example6"];
+        let datasources = comp.swac_comp.data;
+        let mydatasource = datasources['rewards_data']
+        let json_data = mydatasource.getSets();
+        console.log("JSON-Data: ", json_data);
+
+        if (Array.isArray(json_data)) {
+            json_data.forEach(function (set) {
+                let convertedSet = {
+                    title: set.title,
+                    medal: set.medal,
+                    besitz: set.besitz
+                };
+                data.push(convertedSet);
+            });
+
+            // displayRewards(data);
+        } else {
+            console.error("json_data ist kein Array.");
+        }
     }, "present_example6");
 });
+
+/*
+function displayRewards(rewards_list) {
+    
+    // var super_container = document.querySelector(".present_example6");
+    // super_container.innerHTML = "";
+    // var container = document.querySelector(".swac_repeatForSet");
+    
+    var container = document.querySelector(".swac_repeatForSet");
+    container.innerHTML = "";
+
+    rewards_list.forEach(function(reward) {
+        var rewardDiv = document.createElement("div");
+        rewardDiv.classList.add("reward");
+
+        var titleDiv = document.createElement("div");
+        titleDiv.classList.add("title");
+        var title = document.createElement("h4");
+        title.textContent = reward.title;
+        titleDiv.appendChild(title)
+
+        rewardDiv.appendChild(titleDiv);
+
+        container.appendChild(rewardDiv);
+    });
+}
+*/
+
+/*
+function sortRewardsByMedal() {
+    const medalOrder = { Gold: 1, Silber: 2, Bronze: 3 };
+    data.sort((a, b) => medalOrder[a.medal] - medalOrder[b.medal]);
+    displayRewards(data); // Aktualisierte Liste anzeigen
+  }
+  
+  function sortRewardsByOwnership() {
+    data.sort((a, b) => {
+        if (a.besitz && !b.besitz) {
+            return -1;
+        } else if (!a.besitz && b.besitz) {
+            return 1;
+        } else {
+            return 0;
+        }
+    });
+  
+    displayRewards(data); // Aktualisierte Liste anzeigen
+  }
+
+function handleSort() {
+    var sortOptions = document.getElementById("sortOptions");
+    var selectedOption = sortOptions.value;
+  
+    if (selectedOption === "medal") {
+        sortRewardsByMedal();
+    } else if (selectedOption === "besitz") {
+        sortRewardsByOwnership();
+    }
+  }
+*/ 
