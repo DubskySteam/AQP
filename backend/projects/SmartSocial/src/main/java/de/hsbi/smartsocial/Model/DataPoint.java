614b4dcb3ddb5e024de28c568d8057d1afbab331
Dubsky <23001099+DubskySteam@users.noreply.github.com>
2023-12-11 13:57:31 +0100

    update DataPoint

--- backend/projects/SmartSocial/src/main/java/de/hsbi/smartsocial/Model/DataPoint.java
+++ backend/projects/SmartSocial/src/main/java/de/hsbi/smartsocial/Model/DataPoint.java
@@ -3,13 +3,29 @@ package de.hsbi.smartsocial.Model;
 public class DataPoint {
     private String latitude;
     private String longitude;
+    private String ts;
 
     public String getLatitude() {
         return latitude;
     }
 
+    public void setLatitude(String latitude) {
+        this.latitude = latitude;
+    }
+
     public String getLongitude() {
         return longitude;
     }
-}
 
+    public void setLongitude(String longitude) {
+        this.longitude = longitude;
+    }
+
+    public String getTs() {
+        return ts;
+    }
+
+    public void setTs(String ts) {
+        this.ts = ts;
+    }
+}
