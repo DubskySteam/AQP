431bacf87294093fccf90bc84d36d0905f6e90e8
Dubsky <23001099+DubskySteam@users.noreply.github.com>
2023-12-11 13:57:46 +0100

    added SmartData Call && Parsing

--- backend/projects/SmartSocial/src/main/java/de/hsbi/smartsocial/Controller/UtilityController.java
+++ backend/projects/SmartSocial/src/main/java/de/hsbi/smartsocial/Controller/UtilityController.java
@@ -1,5 +1,14 @@
 package de.hsbi.smartsocial.Controller;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.javadocmd.simplelatlng.LatLng;
+import com.javadocmd.simplelatlng.LatLngTool;
+import com.javadocmd.simplelatlng.util.LengthUnit;
+import de.hsbi.smartsocial.Exceptions.APICallException;
+import de.hsbi.smartsocial.Exceptions.ParseJsonArrayException;
+import de.hsbi.smartsocial.Model.DataPoint;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
@@ -7,13 +16,13 @@ import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
 
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
 import java.net.URL;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Objects;
-import java.util.Properties;
+import java.util.*;
 import java.util.jar.Attributes;
 import java.util.jar.Manifest;
 
@@ -61,10 +70,95 @@ public class UtilityController {
     @GET
     @Path("/refreshData")
     @Produces(MediaType.APPLICATION_JSON)
-    @ApiResponse(responseCode = "200", description = "Refreshes the travel data by pulling it from SmartData")
     public Response refreshData() {
+        String jsonArrayString = makeApiCall("http://localhost:8080/SmartDataAirquality/smartdata/records/airquality_a1");
 
+        List<DataPoint> dataPoints = parseJsonArray(jsonArrayString);
+
+        double totalDistance = calculateTotalDistance(dataPoints);
+
+        return Response.ok(dataPoints).build();
     }
 
+    private String makeApiCall(String apiUrl) {
+        StringBuilder response = new StringBuilder();
+
+        try {
+            URL url = new URL(apiUrl);
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+
+            connection.setRequestMethod("GET");
+
+            int responseCode = connection.getResponseCode();
+
+            if (responseCode == HttpURLConnection.HTTP_OK) {
+                BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+                String inputLine;
+
+                while ((inputLine = in.readLine()) != null) {
+                    response.append(inputLine);
+                }
+
+                in.close();
+            } else {
+                throw new APICallException("Error: API call failed with response code " + responseCode);
+            }
+
+            connection.disconnect();
+        } catch (IOException e) {
+            throw new APICallException("Error: API call failed with message " + e.getMessage());
+        }
+
+        return response.toString();
+    }
+
+    private List<DataPoint> parseJsonArray(String jsonArrayString) {
+        List<DataPoint> dataPoints = new ArrayList<>();
+
+        try {
+            ObjectMapper objectMapper = new ObjectMapper();
+            JsonNode root = objectMapper.readTree(jsonArrayString);
+
+            if (root.has("records")) {
+                JsonNode recordsNode = root.get("records");
+
+                for (JsonNode recordNode : recordsNode) {
+                    DataPoint simpleDataPoint = new DataPoint();
+                    simpleDataPoint.setLatitude(recordNode.get("latitude").asText());
+                    simpleDataPoint.setLongitude(recordNode.get("longitude").asText());
+                    simpleDataPoint.setTs(recordNode.get("ts").asText());
+                    dataPoints.add(simpleDataPoint);
+                }
+            } else {
+                throw new ParseJsonArrayException("Error: JSON array does not contain records");
+            }
+        } catch (IOException e) {
+            throw new ParseJsonArrayException("Error: JSON array could not be parsed with message " + e.getMessage());
+        }
+
+        return dataPoints;
+    }
+
+
+    private double calculateTotalDistance(List<DataPoint> dataPoints) {
+        double totalDistance = 0;
+
+        for (int i = 0; i < dataPoints.size() - 1; i++) {
+            DataPoint startDataPoint = dataPoints.get(i);
+            DataPoint endDataPoint = dataPoints.get(i + 1);
+
+            LatLng startLatLng = new LatLng(Double.parseDouble(startDataPoint.getLatitude()),
+                    Double.parseDouble(startDataPoint.getLongitude()));
+
+            LatLng endLatLng = new LatLng(Double.parseDouble(endDataPoint.getLatitude()),
+                    Double.parseDouble(endDataPoint.getLongitude()));
+
+            double distance = LatLngTool.distance(startLatLng, endLatLng, LengthUnit.KILOMETER);
+
+            totalDistance += distance;
+        }
+
+        return totalDistance;
+    }
 
 }
