7cfa8fcb3bc70f0e75f19fe87d9e52eb578dc01b
Dubsky <23001099+DubskySteam@users.noreply.github.com>
2024-01-03 05:50:29 +0100

    added device endpoints

--- backend/projects/SmartSocial/src/main/java/de/hsbi/smartsocial/Controller/ProfileSettingsController.java
+++ backend/projects/SmartSocial/src/main/java/de/hsbi/smartsocial/Controller/ProfileSettingsController.java
@@ -4,13 +4,11 @@ import de.hsbi.smartsocial.Exceptions.ProfileSettingsNotFoundException;
 import de.hsbi.smartsocial.Model.ProfileSetting;
 import de.hsbi.smartsocial.Service.GroupService;
 import de.hsbi.smartsocial.Service.ProfileSettingsService;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import jakarta.inject.Inject;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
-import jakarta.ws.rs.GET;
-import jakarta.ws.rs.POST;
-import jakarta.ws.rs.Path;
-import jakarta.ws.rs.PathParam;
+import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.Response;
 
 /**
@@ -36,6 +34,8 @@ public class ProfileSettingsController {
 
     @GET
     @Path("/getSettings/{id}")
+    @Produces("application/json")
+    @ApiResponse(responseCode = "200", description = "Returns the settings for the given id")
     public Response getSettings(@PathParam("id") Long id) {
         ProfileSetting profileSetting = profileSettingsService.getSettings(id);
         if (profileSetting == null) {
@@ -46,6 +46,9 @@ public class ProfileSettingsController {
 
     @POST
     @Path("/setSettings/{id}")
+    @Produces("application/json")
+    @Consumes("application/json")
+    @ApiResponse(responseCode = "200", description = "Sets the settings for the given id")
     public ProfileSetting setSettings(@PathParam("id") Long id, ProfileSetting profileSetting) {
         ProfileSetting tmp = profileSettingsService.setSettings(id, profileSetting);
         if (tmp == null) {
@@ -56,6 +59,8 @@ public class ProfileSettingsController {
 
     @GET
     @Path("/getVisibility/{id}")
+    @Produces("application/json")
+    @ApiResponse(responseCode = "200", description = "Returns the visibility for the given id")
     public String getVisibilityById(@PathParam("id") Long id) {
         String visibility = profileSettingsService.getVisibility(id);
         if (visibility == null) {
@@ -66,6 +71,8 @@ public class ProfileSettingsController {
 
     @POST
     @Path("/setVisibility/{id}/{mode}")
+    @Produces("application/json")
+    @ApiResponse(responseCode = "200", description = "Sets the visibility for the given id")
     public String setVisibilityById(@PathParam("id") Long id, @PathParam("mode") boolean mode) {
         String visibility = profileSettingsService.setVisibility(id, mode);
         if (visibility == null) {
@@ -76,6 +83,8 @@ public class ProfileSettingsController {
 
     @GET
     @Path("/getPicture/{id}")
+    @Produces("application/json")
+    @ApiResponse(responseCode = "200", description = "Returns the image for the given id")
     public String getPicture(@PathParam("id") Long id) {
         String picture = profileSettingsService.getPicture(id);
         if (picture == null) {
@@ -86,10 +95,36 @@ public class ProfileSettingsController {
 
     @POST
     @Path("/setPicture/{id}/{picture}")
+    @Produces("application/json")
+    @ApiResponse(responseCode = "200", description = "Sets the image for the given id")
     public String setPicture(@PathParam("id") Long id, @PathParam("picture") String picture) {
         String tmp = profileSettingsService.setPicture(id, picture);
         if (tmp == null) {
             throw new ProfileSettingsNotFoundException("Error while setting image for id " + id);
+        }
+        return Response.ok(tmp).build().toString();
+    }
+
+    @GET
+    @Path("/getDevice/{id}")
+    @Produces("application/json")
+    @ApiResponse(responseCode = "200", description = "Returns the device for the given id")
+    public String getDevice(@PathParam("id") Long id) {
+        String device = profileSettingsService.getDevice(id);
+        if (device == null) {
+            throw new ProfileSettingsNotFoundException("No settings(device) found for id " + id);
+        }
+        return Response.ok(device).build().toString();
+    }
+
+    @POST
+    @Path("/setDevice/{id}/{device}")
+    @Produces("application/json")
+    @ApiResponse(responseCode = "200", description = "Sets the device for the given id")
+    public String setDevice(@PathParam("id") Long id, @PathParam("device") String device) {
+        String tmp = profileSettingsService.setDevice(id, device);
+        if (tmp == null) {
+            throw new ProfileSettingsNotFoundException("Error while setting device for id " + id);
         }
         return Response.ok(tmp).build().toString();
     }
