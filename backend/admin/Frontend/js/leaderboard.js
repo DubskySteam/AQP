5c97f9f1ab295ab502cebc49cd3d61fca6651ec7
Dubsky <23001099+DubskySteam@users.noreply.github.com>
2024-01-05 02:58:42 +0100

    moved admin frontend to frontend directory

---
+++ backend/admin/Frontend/js/leaderboard.js
@@ -0,0 +1,30 @@
+function loadLeaderboard() {
+    fetch('http://localhost:8080/SmartSocial/api/leaderboard/getList/30')
+        .then(response => response.json())
+        .then(data => {
+            createLeaderboardTable(data);
+        })
+        .catch(error => {
+            console.error('Error fetching leaderboard data:', error);
+        });
+}
+
+function createLeaderboardTable(data) {
+    const tableContainer = document.getElementById('leaderboard-table');
+    let tableHTML = '<table class="responsetable"><thead><tr><th>ID</th><th>Username</th><th>Finished Quests</th><th>Kilometers</th></tr></thead><tbody>';
+
+    data.forEach(item => {
+        tableHTML += `<tr>
+                        <td>${item.id}</td>
+                        <td>${item.users.username}</td>
+                        <td>${item.finishedQuests}</td>
+                        <td>${item.kilometers}</td>
+                      </tr>`;
+    });
+
+    tableHTML += '</tbody></table>';
+    tableContainer.innerHTML = tableHTML;
+}
+
+
+window.onload = loadLeaderboard;
---
+++ backend/admin/Frontend/js/manager.js
@@ -0,0 +1,46 @@
+async function checkApplicationStatus(appUrl) {
+    try {
+        const response = await fetch(appUrl, { method: 'HEAD' });
+        return response.ok ? "Online" : "Offline";
+    } catch (error) {
+        return "Offline";
+    }
+}
+
+function createApplicationCard(appName, appUrl) {
+    const card = document.createElement('div');
+    card.classList.add('card');
+
+    const header = document.createElement('div');
+    header.classList.add('card-header');
+    header.textContent = appName;
+
+    const body = document.createElement('div');
+    body.classList.add('card-body');
+
+    checkApplicationStatus(appUrl).then(status => {
+        body.textContent = status;
+        body.classList.add(status === "Offline" ? 'offline' : 'online');
+    });
+
+    card.appendChild(header);
+    card.appendChild(body);
+
+    return card;
+}
+
+async function displayApplicationStatuses() {
+    const container = document.getElementById('application-container'); // Ensure this container exists in your HTML
+    container.innerHTML = '';
+
+    const response = await fetch('http://localhost:8080/Admin/api/application/getApplications'); // Replace with your API endpoint
+    const applications = await response.json();
+
+    Object.keys(applications).forEach(appName => {
+        const appUrl = applications[appName];
+        const card = createApplicationCard(appName, appUrl);
+        container.appendChild(card);
+    });
+}
+
+window.onload = displayApplicationStatuses;
