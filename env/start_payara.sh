fdedc47ab0e6ddb341fa1b6d272f33fd2be6369b
Dubsky <23001099+DubskySteam@users.noreply.github.com>
2024-01-13 15:44:33 +0100

    update env scripts

--- env/start_payara.sh
+++ env/start_payara.sh
@@ -2,5 +2,11 @@
 PAYARA_HOME=payara6
 DOMAIN_NAME=domain1
 
-echo "Starting Payara Server domain: $DOMAIN_NAME"
-$PAYARA_HOME/bin/asadmin start-domain $DOMAIN_NAME
+if $PAYARA_HOME/bin/asadmin list-domains | grep -q "$DOMAIN_NAME running"; then
+    echo "Stopping Payara Server domain: $DOMAIN_NAME"
+    $PAYARA_HOME/bin/asadmin stop-domain $DOMAIN_NAME
+else
+    echo "Starting Payara Server domain: $DOMAIN_NAME"
+    $PAYARA_HOME/bin/asadmin start-domain $DOMAIN_NAME
+fi
+
