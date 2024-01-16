c526b31b8859c9487d97d208a295598be803d7d1
Dubsky <23001099+DubskySteam@users.noreply.github.com>
2024-01-16 08:41:17 +0100

    update scripts

---
+++ backend/scripts/smartsocial_schema.sql
@@ -0,0 +1,3 @@
+-- DROP SCHEMA smartsocial;
+
+CREATE SCHEMA smartsocial AUTHORIZATION smartuser;
--- backend/scripts/smartsocials.sql
+++ backend/scripts/smartsocials.sql
@@ -1,69 +1,153 @@
--- Create the smartsocial schema
-CREATE SCHEMA smartsocial;
+-- smartsocial.achievements definition
 
--- Create Groups table
-CREATE TABLE smartsocial.Groups (
-    group_id SERIAL PRIMARY KEY,
-    name VARCHAR(255) NOT NULL,
-    creation_date DATE NOT NULL,
-    admin_user_id INT REFERENCES smartuser.users(id),
-    description TEXT,
-    image VARCHAR(255),
-    code VARCHAR(255)
+-- Drop table
+
+-- DROP TABLE smartsocial.achievements;
+
+CREATE TABLE smartsocial.achievements (
+	achievement_id serial4 NOT NULL,
+	"name" varchar(255) NOT NULL,
+	description text NULL,
+	CONSTRAINT achievements_pkey PRIMARY KEY (achievement_id)
 );
 
--- Create GroupMembers table
-CREATE TABLE smartsocial.GroupMembers (
-    group_id INT REFERENCES smartsocial.Groups(group_id),
-    user_id INT REFERENCES smartuser.users(id),
-    member_since DATE NOT NULL,
-    status VARCHAR(100) NOT NULL,
-    PRIMARY KEY (group_id, user_id)
+
+-- smartsocial.quests definition
+
+-- Drop table
+
+-- DROP TABLE smartsocial.quests;
+
+CREATE TABLE smartsocial.quests (
+	quest_id serial4 NOT NULL,
+	description text NOT NULL,
+	xp_reward int4 NOT NULL,
+	"type" varchar NULL,
+	amount int4 NULL,
+	title varchar NULL,
+	CONSTRAINT quests_pkey PRIMARY KEY (quest_id)
 );
 
--- Create Achievements table
-CREATE TABLE smartsocial.Achievements (
-    achievement_id SERIAL PRIMARY KEY,
-    name VARCHAR(255) NOT NULL,
-    description TEXT,
-    date_of_receivement DATE NOT NULL
+
+-- smartsocial.groupmembers definition
+
+-- Drop table
+
+-- DROP TABLE smartsocial.groupmembers;
+
+CREATE TABLE smartsocial.groupmembers (
+	group_id int4 NOT NULL,
+	user_id int4 NOT NULL,
+	member_since date NOT NULL,
+	status varchar(100) NOT NULL,
+	CONSTRAINT groupmembers_pkey PRIMARY KEY (group_id, user_id)
 );
 
--- Create UserAchievements table
-CREATE TABLE smartsocial.UserAchievements (
-    user_id INT REFERENCES smartuser.users(id),
-    achievement_id INT REFERENCES smartsocial.Achievements(achievement_id),
-    PRIMARY KEY (user_id, achievement_id)
+
+-- smartsocial."groups" definition
+
+-- Drop table
+
+-- DROP TABLE smartsocial."groups";
+
+CREATE TABLE smartsocial."groups" (
+	group_id serial4 NOT NULL,
+	"name" varchar(255) NOT NULL,
+	creation_date date NOT NULL,
+	admin_user_id int4 NULL,
+	description text NULL,
+	image varchar NULL,
+	code varchar NULL,
+	CONSTRAINT groups_pkey PRIMARY KEY (group_id)
 );
 
--- Create ProfileSettings table
-CREATE TABLE smartsocial.ProfileSettings (
-    user_id INT PRIMARY KEY REFERENCES smartuser.users(id),
-    profile_visibility VARCHAR(50) NOT NULL,
-    notifications_enabled BOOLEAN NOT NULL,
-    additional_settings JSONB,
-    picture VARCHAR(255),
-    device VARCHAR(100)
+
+-- smartsocial.leaderboard definition
+
+-- Drop table
+
+-- DROP TABLE smartsocial.leaderboard;
+
+CREATE TABLE smartsocial.leaderboard (
+	userid int4 NOT NULL,
+	kilometers numeric(10, 2) NOT NULL,
+	finished_quests int4 NOT NULL,
+	CONSTRAINT leaderboard_pkey PRIMARY KEY (userid)
 );
 
--- Create Quests table
-CREATE TABLE smartsocial.Quests (
-    quest_id SERIAL PRIMARY KEY,
-    description TEXT NOT NULL,
-    xp_reward INT NOT NULL
+
+-- smartsocial.profilesettings definition
+
+-- Drop table
+
+-- DROP TABLE smartsocial.profilesettings;
+
+CREATE TABLE smartsocial.profilesettings (
+	user_id int4 NOT NULL,
+	profile_visibility varchar(50) NOT NULL,
+	notifications_enabled bool NOT NULL,
+	additional_settings jsonb NULL,
+	picture varchar NULL,
+	device varchar NULL,
+	CONSTRAINT profilesettings_pkey PRIMARY KEY (user_id)
 );
 
--- Create UserQuests table
-CREATE TABLE smartsocial.UserQuests (
-    user_id INT REFERENCES smartuser.users(id),
-    quest_id INT REFERENCES smartsocial.Quests(quest_id),
-    completion_date DATE,
-    PRIMARY KEY (user_id, quest_id)
+
+-- smartsocial.userachievements definition
+
+-- Drop table
+
+-- DROP TABLE smartsocial.userachievements;
+
+CREATE TABLE smartsocial.userachievements (
+	user_id int4 NOT NULL,
+	achievement_id int4 NOT NULL,
+	CONSTRAINT userachievements_pkey PRIMARY KEY (user_id, achievement_id)
 );
 
-CREATE TABLE SmartSocial.leaderboard (
-    userid INT REFERENCES smartuser.users(id),
-    kilometers DECIMAL(10, 2) NOT NULL,
-    finished_quests INT NOT NULL,
-    PRIMARY KEY (userid)
+
+-- smartsocial.userquests definition
+
+-- Drop table
+
+-- DROP TABLE smartsocial.userquests;
+
+CREATE TABLE smartsocial.userquests (
+	user_id int4 NOT NULL,
+	quest_id int4 NOT NULL,
+	completion_date date NULL,
+	CONSTRAINT userquests_pkey PRIMARY KEY (user_id, quest_id)
 );
+
+
+-- smartsocial.groupmembers foreign keys
+
+ALTER TABLE smartsocial.groupmembers ADD CONSTRAINT groupmembers_group_id_fkey FOREIGN KEY (group_id) REFERENCES smartsocial."groups"(group_id);
+ALTER TABLE smartsocial.groupmembers ADD CONSTRAINT groupmembers_user_id_fkey FOREIGN KEY (user_id) REFERENCES smartuser.users(id);
+
+
+-- smartsocial."groups" foreign keys
+
+ALTER TABLE smartsocial."groups" ADD CONSTRAINT groups_admin_user_id_fkey FOREIGN KEY (admin_user_id) REFERENCES smartuser.users(id);
+
+
+-- smartsocial.leaderboard foreign keys
+
+ALTER TABLE smartsocial.leaderboard ADD CONSTRAINT leaderboard_userid_fkey FOREIGN KEY (userid) REFERENCES smartuser.users(id);
+
+
+-- smartsocial.profilesettings foreign keys
+
+ALTER TABLE smartsocial.profilesettings ADD CONSTRAINT profilesettings_user_id_fkey FOREIGN KEY (user_id) REFERENCES smartuser.users(id);
+
+
+-- smartsocial.userachievements foreign keys
+
+ALTER TABLE smartsocial.userachievements ADD CONSTRAINT userachievements_achievement_id_fkey FOREIGN KEY (achievement_id) REFERENCES smartsocial.achievements(achievement_id);
+ALTER TABLE smartsocial.userachievements ADD CONSTRAINT userachievements_user_id_fkey FOREIGN KEY (user_id) REFERENCES smartuser.users(id);
+
+
+-- smartsocial.userquests foreign keys
+
+ALTER TABLE smartsocial.userquests ADD CONSTRAINT userquests_quest_id_fkey FOREIGN KEY (quest_id) REFERENCES smartsocial.quests(quest_id);
+ALTER TABLE smartsocial.userquests ADD CONSTRAINT userquests_user_id_fkey FOREIGN KEY (user_id) REFERENCES smartuser.users(id);
